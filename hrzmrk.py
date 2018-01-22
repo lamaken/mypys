88f0f544d886f2f87b38bc9c6e3a726d8886087f
Alejo Campos <lamaken@gmail.com>
2018-01-22 10:28:18 +0100

    version 0.1

--- hrzmrk.py
+++ hrzmrk.py
@@ -1,45 +1,54 @@
 #!/usr/bin/env python
 
-from PIL import Image
+import PIL.Image as Image
 
 
 #horitzontal
 
+resources = "resources/"
 param = 1
 aureo = param * 1.618033988
 
 
+# canvas size
 height = 291
-
-
 width = int (height *  aureo)
-
-resources = "resources/"
-
-
 skysize 	= (width,int((height/ aureo)+1))
 terrainsize = (width,int(height-(height/ aureo)))
 hrzsize		= (width,height)
 sunsize 	=  skysize
 
+
+# elements
+
 sky 	= Image.open(resources+"sky.png").resize(skysize , Image.ANTIALIAS)
 terrain = Image.open(resources+"terrain.png").resize(terrainsize , Image.ANTIALIAS)
-hrzimg 	= Image.open(resources+"blanco.png").resize(hrzsize , Image.ANTIALIAS)
 sun	= Image.open(resources+"sun.png").resize(sunsize , Image.ANTIALIAS)
 
-tempimg=terrain.copy()
 
+# filter colors
+
+mascaraR	= Image.open(resources+"red.png").resize(hrzsize , Image.ANTIALIAS)
+mascaraG	= Image.open(resources+"green.png").resize(hrzsize , Image.ANTIALIAS)
+mascaraB	= Image.open(resources+"blue.png").resize(hrzsize , Image.ANTIALIAS)
+
+
+#destination 
+
+hrzimg 	= Image.open(resources+"blanco.png").resize(hrzsize , Image.ANTIALIAS)
 
 skypos = (0,0)
 terrainpos = (0,int((height/aureo)+1))
-sunpos = (20,0)
+sunpos = (0,0)
 
 hrzimg.paste(sky, skypos, sky)
 hrzimg.paste(sun, sunpos, sun)
 hrzimg.paste(terrain, terrainpos, terrain)
 
-hrzimg.save('hrz3.gif')
-hrzimg.show()
+
+# blend with alpha=0.5
+result = Image.blend(hrzimg, hrzimg, alpha=0.5)
+
 """
 creo la mascara de color per a tot el dibuix
 
@@ -55,3 +64,5 @@ position = (10,10)
 image.paste(sunResized, position, sunResized)
 image_copy.save('hrz2.png')
 """
+hrzimg.save('hrz3.gif')
+hrzimg.show()
