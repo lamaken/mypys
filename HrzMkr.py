2c1cd7df8bcce0dc1e9873eb18e9769112b732ea
root <lamaken@gmail.com>
2018-02-10 10:24:18 +0100

    pyHorizonMakerv0.1

---
+++ HrzMkr.py
@@ -0,0 +1,36 @@
+class HrzMkr:
+    def __init__(self):
+        self.datos = []
+    def agregar(self):
+        self.datos.append()
+    def setNameHrz(self,name):
+    	self.NameHrz=name
+	def setdtHrz(self,name):
+    	self.NameHrz=name
+	def settopHrz(self,name):
+    	self.NameHrz=name
+	def settopHrzColor(self,name):
+    	self.NameHrz=name
+	def bottomHrzColor(self,name):
+    	self.NameHrz=name
+	def setNameHrz(self,name):
+    	self.NameHrz=name
+	def setNameHrz(self,name):
+    	self.NameHrz=name
+
+
+
+    	nameHrz,
+dtHrz,
+topHrz,
+topHrzColor,
+bottomHrzColor,
+widthHrz,
+heightHrz,
+authorHrz,
+xPal,yPal,hPalx,hPaly,
+alcada,
+colPal,
+superX,
+superY,
+version
--- hrzmkr.py
+++ hrzmkr.py
@@ -21,136 +21,154 @@ import os
 tempdir = "/var/www/html/temp/"      
 
 def random_color():
-      levels = range(10,150,10)
-      return tuple(random.choice(levels) for _ in range(3))
+	levels = range(10,250,10)
+	return tuple(random.choice(levels) for _ in range(3))
 
 def random_pos(m,n):
-      r = random.choice(range(m,n))
-      return r 
+	r = random.choice(range(m,n))
+	return r 
 
 def genImage():
-      #horitzontal
-      resources = "/var/www/html/resources/"
-      param = 1
-      
-      aureo = param * 1.618033988
+	#horitzontal
+	resources = "/var/www/html/resources/"
+	param = 1
+	
+	aureo = param * 1.618033988
 
-      # canvas size
-      height = 333#random.choice((100,300,1))
-      width = int (height *  aureo)
-      skysize     = (width,int((height/ aureo)+1))
-      terrainsize = (width,int(height-(height/ aureo)))
-      hrzsize     = (width,height)
-      sunsize     = (height,height)
-      
-      # elemens
-      #sky
-      skypos = (0,0)
-      skyColor = random_color()
-      sky     = Image.open(resources+"sky.png").resize(skysize , Image.ANTIALIAS)
-      skyMasc = Image.new('RGBA', skysize, skyColor)
+	# canvas size
+	height = 333#random.choice((100,300,1))
+	width = int (height *  aureo)
+	skysize     = (width,int((height/ aureo)+1))
+	terrainsize = (width,int(height-(height/ aureo)))
+	hrzsize     = (width,height)
+	sunsize     = (height,height)
+	
+	# elemens
+	#sky
+	skypos = (0,0)
+	skyColor = random_color()
+	sky     = Image.open(resources+"sky.png").resize(skysize , Image.ANTIALIAS)
+	skyMasc = Image.new('RGBA', skysize, skyColor)
 
-      #terrain
-      terrainpos = (0,int((height/aureo)+1))
-      terrainColor = random_color()
-      terrain = Image.open(resources+"terrain.png").resize(terrainsize , Image.ANTIALIAS)
-      terrainMasc = Image.new('RGBA', terrainsize, terrainColor)
+	#terrain
+	terrainpos = (0,int((height/aureo)+1))
+	terrainColor = random_color()
+	terrain = Image.open(resources+"terrain.png").resize(terrainsize , Image.ANTIALIAS)
+	terrainMasc = Image.new('RGBA', terrainsize, terrainColor)
 
-      #terrain shadow
-      shadow = Image.open(resources+"shadow.png").resize(terrainsize , Image.ANTIALIAS)
-      terrain = Image.blend(terrain,shadow,alpha=0.5)
+	#terrain shadow
+	shadow = Image.open(resources+"shadow.png").resize(terrainsize , Image.ANTIALIAS)
+	terrain = Image.blend(terrain,shadow,alpha=0.2)
 
-      #marc
-      marc = Image.open(resources+"border.png").resize(hrzsize , Image.ANTIALIAS)
-      marcpos = (0,0)
+	#marc
+	marc = Image.open(resources+"border.png").resize(hrzsize , Image.ANTIALIAS)
+	marcpos = (0,0)
 
-      #sun
-      sunpos = (0,0)
-      
+	#sun
+	sunpos = (0,0)
+	
 
-      sunpos2 = (int(-height/2.3),int(-height/2.3))
-      randomsunpos = (random_pos(0,width),random_pos(0,int((height/ aureo)+1)))
-      
+	sunpos2 = (int(-height/2),int(-height/2))
+	randomsunpos = (random_pos(0,width),random_pos(0,int((height/ aureo)+1)))
+	
 
-      sunImgpos = tuple(map(sum, zip(sunpos2,randomsunpos)))
+	sunImgpos = tuple(map(sum, zip(sunpos2,randomsunpos)))
 
-      sunColor = random_color()
-      sun = Image.open(resources+"sun.png").resize(sunsize , Image.ANTIALIAS)
-      sunMasc = Image.new('RGBA', skysize,sunColor)
-      sunImg = sun.resize(sunsize)
+	sunColor = random_color()
+	sun = Image.open(resources+"sun.png").resize(sunsize , Image.ANTIALIAS)
+	sunMasc = Image.new('RGBA', skysize,sunColor)
+	sunImg = sun.resize(sunsize)
 
-      #destination 
-      hrzimg = Image.new("RGBA",hrzsize,(0,0,0,255))
+	#destination 
+	hrzimg = Image.new("RGBA",hrzsize,(0,0,0,255))
 
 
-      sunMasc.paste(sunImg, sunImgpos, sunImg)
+	sunMasc.paste(sunImg, sunImgpos, sunImg)
 
-      skyImg = Image.blend(skyMasc,sky,alpha=0.50)
-      sunImg = Image.blend(skyImg,sunMasc,alpha=0.50)
-      terrainImg=Image.blend(terrainMasc,terrain,alpha=0.50)
+	skyImg = Image.blend(skyMasc,sky,alpha=0.80)
+	sunImg = Image.blend(skyImg,sunMasc,alpha=0.80)
+	terrainImg=Image.blend(terrainMasc,terrain,alpha=0.80)
 
-      skyImg.paste(sunImg,sunpos)
-      hrzimg.paste(skyImg,skypos)
-      hrzimg.paste(terrainImg,terrainpos,terrainImg)
+	skyImg.paste(sunImg,sunpos)
+	hrzimg.paste(skyImg,skypos)
+	hrzimg.paste(terrainImg,terrainpos,terrainImg)
 
-      
+	
      
-      im = ImageDraw.Draw(hrzimg)
+	im = ImageDraw.Draw(hrzimg)
 
 
      
 
-      #monolito vaya
-      
+	#monolito vaya
+	
 
-      #bright
-      h = height-(height/ aureo)
-      tall=random_pos(1,int(h))
-      #default len
-      y=int((height/ aureo)+1)+random_pos( 0 ,int(height-(height/ aureo))-tall)
+	#bright
+	h = height-(height/ aureo)
+	tall=random_pos(1,int(h))
+	#default len
+	y=int((height/ aureo)+1)+random_pos( 0 ,int(height-(height/ aureo))-tall)
 
-      
-      palrandompos = ( random_pos(0, width), y) 
-      
-      #change if far
-      #tall = (tall*h)  % (y/h)
-      
-      
+	
+	palrandompos = ( random_pos(0, width), y) 
+	
+	#change if far
+	#tall = (tall*h)  % (y/h)
+	
+	
 
-      palpos = tuple(map(sum, zip((0,-tall),palrandompos)))
-      im.line((palrandompos,palpos),(200,200,200,200),2)
+	palpos = tuple(map(sum, zip((0,-tall),palrandompos)))
+	im.line((palrandompos,palpos),(200,200,200,200),2)
 
 
-      #hide
-      palpos = tuple(map(sum, zip((2,-tall),palrandompos)))
-      palrandompos = tuple(map(sum, zip((2,0),palrandompos)))
-      im.line((palpos,palrandompos),(0,0,0,250),2)
-      
-      #shadow
-      shadowpos = tuple(map(sum, zip((20,10),palrandompos)))
-      im.line( (palrandompos,shadowpos) , (0,0,0,225) , 5 )
-      del im
+	#hide
+	palpos = tuple(map(sum, zip((1,-tall),palrandompos)))
+	palrandompos = tuple(map(sum, zip((1,0),palrandompos)))
+	im.line((palpos,palrandompos),(0,0,0,250),2)
+	
+	#shadow
+	shadowX=sunImgpos[0] 
 
-      #ponemos el marco
-      hrzimg.paste(marc,marcpos,marc)
+	shadowpos = tuple(map(sum, zip((shadowX,10),palrandompos)))
+	im.line( (palrandompos,shadowpos) , (0,0,0,225) , 5 )
+	del im
 
-      #ponemos etiqueta
-      im = ImageDraw.Draw(hrzimg)
-      im.text((200,y),"y="+str(y),"white")
-      im.text(sunImgpos,"*","white")      
-      im.text(palrandompos,"*","white")
-      im.text((100,100),"tall="+str(tall),"white")
-      del im
-      
-     
-      filename=tempdir+str(uuid.uuid4())+".png"
-      print filename
-      
-      hrzimg.save(filename)
-      data = open(filename, "rb").read()
-      os.remove(filename)
+	#ponemos el marco
+	hrzimg.paste(marc,marcpos,marc)
 
-      return data
+
+		
+	#ponemos etiqueta
+
+	unik=str(uuid.uuid4())[:8];
+
+	im = ImageDraw.Draw(hrzimg)
+	#im.text((200,y),"y="+str(y),"white")
+	#im.text(sunpos2,"*","white")      
+	#im.text(palrandompos,"*","white")
+	#im.text((200,204),"shadowX="+str(shadowX)+" sunImgpos="+str(sunImgpos[0]),"white")      
+	#im.text((200,234),"sunpos="+str(sunpos2[0])+","+str(sunpos2[1]),"white")      
+	
+	#im.text((100,100),"tall="+str(tall),"white")
+	im.text( (width/2-10,height-10),unik,"black")
+	im.text( (width/2-10,height-11),unik,"pink")
+
+
+	im.text( (width-86,height-30)," by hrzmkr","olive")
+
+	im.text( (width-86,height-31)," by hrzmkr","lime")
+	
+	del im
+	
+	
+	filename=tempdir+unik+".png"
+	print filename
+	
+	hrzimg.save(filename)
+	data = open(filename, "rb").read()
+	#os.remove(filename)
+
+	return data
 
 def handler(req):
     data = genImage()
@@ -162,3 +180,20 @@ def handler(req):
 
 print "Only to handle http requests"
 
+
+"""
+nameHrz,
+dtHrz,
+topHrz,
+topHrzColor,
+bottomHrzColor,
+widthHrz,
+heightHrz,
+authorHrz,
+xPal,yPal,hPalx,hPaly,
+alcada,
+colPal,
+superX,
+superY,
+version
+"""
