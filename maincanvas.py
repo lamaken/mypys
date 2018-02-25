6d98bb4d762d9a7373d625e6e29c604441c4491e
Alejo Campos <lamaken@gmail.com>
2018-02-25 02:20:54 +0100

    UNA MIQUETA MES només queda: el pal l'ombra i dibuixar ho

--- maincanvas.py
+++ maincanvas.py
@@ -3,12 +3,14 @@
 
 
 # index points
+import random
 import uuid
-from random import random
 
 from PIL import Image, ImageDraw, ImageFont
 
-resources = "/home/alex/PycharmProjects/mypys/resources/"
+resources = "/var/www/html/resources/"
+output = "/var/www/html/temp/"
+
 
 
 def random_color():
@@ -19,7 +21,6 @@ def random_color():
 class Canvas(object):
 
     def __init__(self, canvas):
-        self.unik = str(uuid.uuid4())[:8]
         self.name = canvas[CANVAS_NAME]
         self.left = canvas[CANVAS_LEFT]
         self.top = canvas[CANVAS_TOP]
@@ -31,14 +32,14 @@ class Canvas(object):
         print('Canvas ', self.name, 'created')
 
     def draw_with_rectangles(self, draw):
-        img_rectangle = ((self.width, self.height), (self.left, self.top))
-        draw.rectangle(img_rectangle, fill=self.color, outline=(0, 0, 0, 255))
+        img_rectangle = ((self.width - 1, self.height - 1), (self.left + 1, self.top + 1))
+        draw.rectangle(img_rectangle, fill=None, outline=(210, 130, 20, 255))
 
     def draw_with_images(self, hrzimg):
         img_size = (self.width - self.left, self.height - self.top)
         mask = Image.new('RGBA', img_size, self.color)
         image = Image.open(resources + self.file_name).resize(img_size, Image.ANTIALIAS)
-        canvas = Image.blend(mask, image, alpha=0.50)
+        canvas = Image.blend(mask, image, alpha=0.44)
         hrzimg.paste(canvas, (self.left, self.top), image)
 
     def draw_with_text(self, draw):
@@ -49,11 +50,15 @@ class Canvas(object):
 class MainCanvas(object):
     def __init__(self, w, h):
         self.allCanvas = []
+        self.unik = str(uuid.uuid4())[:8]
         self.height = h
         self.width = w
-        self.backcolor = (0, 0, 0, 0)
+        self.backcolor = "white"
 
         print("MainCanvas start.")
+
+    def get_filename(self):
+        return output + self.unik + ".png"
 
     def draw_canvas(self):
         canvas = (self.width, self.height)
@@ -61,13 +66,14 @@ class MainCanvas(object):
         draw = ImageDraw.Draw(im)
 
         for index in range(len(self.allCanvas)):
-            # self.allCanvas[index].draw_with_rectangles(draw)
             self.allCanvas[index].draw_with_images(im)
+            # self.allCanvas[index].draw_with_rectangles(draw)
             # self.allCanvas[index].draw_with_text(draw)
 
-        # im.save('/home/alex/PycharmProjects/mypys/temp/im.png')
+        im.save(self.get_filename())
         del draw
-        im.show()
+        data = open(self.get_filename(), "rb").read()
+        return data
 
     def add(self, another_canvas):
         self.allCanvas.append(another_canvas)
@@ -76,8 +82,8 @@ class MainCanvas(object):
 # H.M.T.
 # hand made tests
 
-IMAGE_WIDTH = 800
-IMAGE_HEIGHT = 300
+IMAGE_WIDTH = 666
+IMAGE_HEIGHT = 333
 
 CANVAS_NAME = 0
 CANVAS_LEFT = 1
@@ -88,7 +94,7 @@ CANVAS_FILE_NAME = 5
 CANVAS_COLOR = 6
 
 # -- padding
-PADDING_SIZE = (15, 15, 15, 15)
+PADDING_SIZE = (33, 33, 33, 33)
 
 PADDING_LEFT = 0
 PADDING_TOP = 1
@@ -106,76 +112,83 @@ border = (PADDING_SIZE[PADDING_LEFT],
           IMAGE_HEIGHT - PADDING_SIZE[PADDING_BOTTOM])
 
 # horizon line
-sky_height = border[BORDER_HEIGHT] / 2
-
-img_sky = ("sky",
-           border[BORDER_LEFT],
-           border[BORDER_TOP],
-           border[BORDER_WIDTH],
-           border[BORDER_TOP] + sky_height,
-           "sky.png",
-           "purple",)
-
-img_land = ("land",
-            border[BORDER_LEFT],
-            border[BORDER_TOP] + sky_height,
-            border[BORDER_WIDTH],
-            border[BORDER_HEIGHT],
-            "terrain.png",
-            "cyan",)
+sky_height = int(border[BORDER_HEIGHT] * 1 / 1.618033988)
 
 # SUN >*<
 
-SUN_WIDTH = 280
-SUN_HEIGHT = 280
+SUN_WIDTH = 480
+SUN_HEIGHT = 480
 
 SUN_SIZE = (SUN_WIDTH, SUN_HEIGHT)
 
 SUN_POSITION = (border[BORDER_WIDTH] / 2 - SUN_WIDTH / 2, border[BORDER_TOP] + sky_height / 2 - SUN_HEIGHT / 2)
 
-img_sun = ("sun",
-           SUN_POSITION[0],
-           SUN_POSITION[1],
-           SUN_POSITION[0] + SUN_SIZE[0],
-           SUN_POSITION[1] + SUN_SIZE[1],
-           "sun.png",
-           "red",)
 
-img_bara = ("bara",
-            border[BORDER_WIDTH] / 2,
-            sky_height,
-            border[BORDER_WIDTH] / 2 + 5,
-            sky_height + sky_height / 2,
-            "bara.png",
-            "yellow",)
 
-img_border = ("border",
-              border[BORDER_LEFT],
-              border[BORDER_TOP],
-              border[BORDER_WIDTH],
-              border[BORDER_HEIGHT],
-              "border.png",
-              "green")
+def handler(req):
+    img_sky = ("sky",
+               border[BORDER_LEFT],
+               border[BORDER_TOP],
+               border[BORDER_WIDTH],
+               border[BORDER_TOP] + sky_height,
+               "sky.png",
+               random_color(),)
 
-main = MainCanvas(IMAGE_WIDTH, IMAGE_HEIGHT)
-# main.add(Canvas(img_canvas))
-# main.add(Canvas(img_padding))
-main.add(Canvas(img_sky))
-main.add(Canvas(img_land))
-main.add(Canvas(img_sun))
-main.add(Canvas(img_bara))
+    img_land = ("land",
+                border[BORDER_LEFT],
+                border[BORDER_TOP] + sky_height,
+                border[BORDER_WIDTH],
+                border[BORDER_HEIGHT],
+                "terrain.png",
+                random_color(),)
 
-main.add(Canvas(img_border))
+    img_sun = ("sun",
+               SUN_POSITION[0],
+               SUN_POSITION[1],
+               SUN_POSITION[0] + SUN_SIZE[0],
+               SUN_POSITION[1] + SUN_SIZE[1],
+               "sun.png",
+               random_color(),)
 
-print "1. border"
-print img_border
-print "2. sky"
-print img_sky
-print "3. land"
-print img_land
-print "4. sun"
-print img_sun
-print "5. bara"
-print img_bara
+    img_bara = ("bara",
+                border[BORDER_WIDTH] - SUN_SIZE[1] / 2 - 5,
+                sky_height,
+                border[BORDER_WIDTH] - SUN_SIZE[1] / 2,
+                sky_height + sky_height / 4,
+                "bara.png",
+                random_color(),)
 
-main.draw_canvas()
+    img_bara_shadow = ("shadow",
+                       img_bara[1],
+                       sky_height + sky_height / 4,
+                       img_bara[1] + 105,
+                       sky_height + sky_height / 2,
+                       "bara_shadow.png",
+                       "black",)
+
+    img_border = ("border",
+                  border[BORDER_LEFT],
+                  border[BORDER_TOP],
+                  border[BORDER_WIDTH],
+                  border[BORDER_HEIGHT],
+                  "border.png",
+                  "gold")
+
+    main = MainCanvas(IMAGE_WIDTH, IMAGE_HEIGHT)
+    main.add(Canvas(img_sky))
+    main.add(Canvas(img_land))
+    main.add(Canvas(img_sun))
+    main.add(Canvas(img_bara))
+    main.add(Canvas(img_bara_shadow))
+    main.add(Canvas(img_border))
+    data = main.draw_canvas()
+    req.content_type = "image/png"
+    req.content_length = str(len(data))
+    req.write(data)
+
+    return req.OK
+
+
+
+
+
